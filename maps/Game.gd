b08b84d224da7fbe282dd8e75b02dda00726b03d
Nikita Blizniuk <bliznyuknm@gmail.com>
2022-05-15 09:09:09 +0200

    added UI

--- maps/Game.gd
+++ maps/Game.gd
@@ -1,24 +1,32 @@
 extends Spatial
 
 
+export var hours: = 336.0
+export var hours_drain_per_second: = 1.0
+
+
 onready var traveller: = $Traveller
+onready var top_bar: = $TopBar
 
 
 func _ready() -> void:
 	traveller.connect("routes_changed", self, "_show_route_selector")
-	$TopBar.update_money(traveller.money)
+	top_bar.update_money(traveller.money)
 	$RouteSelector.visible = false
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if $RouteSelector.visible and Input.is_action_just_pressed("ui_focus_next"):
 		$RouteSelector/Container.roll()
 		
 	if traveller.routes_available.size() > 0 and Input.is_action_just_pressed("traveller_keyboard_select") and $RouteSelector/Container.can_buy_selected():
 		traveller.select_route($RouteSelector/Container.selected_index, $RouteSelector/Container.get_selected_ticket_cost())
-		$TopBar.update_money(traveller.money)
+		top_bar.update_money(traveller.money)
 	
-	$TopBar.update_points(traveller.points)
+	top_bar.update_points(traveller.points)
+	var hours_int: = int(hours)
+	top_bar.update_time(hours_int % 24, hours_int / 24)
+	hours -= hours_drain_per_second * delta
 
 
 func _show_route_selector(traveller: Traveller, routes: Array) -> void:
--- ui/TopBar.gd
+++ ui/TopBar.gd
@@ -2,8 +2,13 @@ extends Control
 
 
 func update_money(count: int) -> void:
-	$Money.text = str(count)
+	$Money/Amount.text = str(count)
 
 
 func update_points(points: int) -> void:
-	$Points.text = str(points)
+	$Points/Amount.text = str(points)
+
+
+func update_time(hours: float, days: int) -> void:
+	$Time/Calendar/Days.text = str(days)
+	$Time/Clock/Pivot.rect_rotation = (1 - hours / 24.0) * 360
